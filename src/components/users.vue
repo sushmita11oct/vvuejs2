86e771eaa5fb1354e218d624b105bae8361743d9
Sushmita Yadav <sushmita.111098@gmail.com>
2021-06-29 14:52:00 +0530

    add all files

---
+++ src/components/users.vue
@@ -0,0 +1,74 @@
+<template>
+<center>
+    <div class="container">     
+        <h1>USER LIST
+            </h1>
+        <table border="1">
+            <thead>
+<tr>
+    <th>id</th>
+     <th>email</th>
+      <th> First NAME</th>
+       <th>Last NAME</th>
+        <th>Avtaar</th>
+    </tr>
+</thead>
+<tbody>
+        <tr>
+          <td>1</td>
+            <td>michael.lawson@reqres.in</td>
+              <td>Michael</td>
+                <td>Lawson</td>
+             <td>   <img src="img_avatar2.png" alt="Avatar" class="avatar"> </td>
+                
+                
+           
+          </tr>
+
+           <tr>
+          <td>2</td>
+            <td>lindsay.ferguson@reqres.in</td>
+              <td>Lindsay</td>
+                <td>Ferguson</td>
+                 <td><img src="img_avatar2.png" alt="Avatar" class="avatar"></td>
+          </tr>
+
+           <tr>
+          <td>3</td>
+            <td>tobias.funke@reqres.in</td>
+              <td>Tobias</td>
+                <td>Funke</td>
+                 <td><img src="img_avatar2.png" alt="Avatar" class="avatar"></td>
+          </tr>
+
+           <tr>
+          <td>4</td>
+            <td>byron.fields@reqres.in</td>
+              <td>Byron</td>
+                <td>Fields</td>
+                 <td><img src="img_avatar2.png" alt="Avatar" class="avatar"></td>
+          </tr>
+
+           <tr>
+          <td>5</td>
+            <td>george.edwards@reqres.in</td>
+              <td>George</td>
+                <td>Edwards</td>
+                 <td><img src="img_avatar2.png" alt="Avatar" class="avatar"></td>
+          </tr>
+
+           <tr>
+          <td>6</td>
+            <td>rachel.howell@reqres.in</td>
+              <td>Rachel</td>
+                <td>Howell</td>
+                 <td><img src="img_avatar2.png" alt="Avatar" class="avatar"></td>
+          </tr>
+
+
+
+</tbody>
+</table>
+</div>
+</center>
+</template>
